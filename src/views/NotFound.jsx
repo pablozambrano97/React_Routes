c31b6856df4bd4c6ac968d504cfe7b41db135013
Pablo <emilio7b@gmail.com>
2023-09-20 20:28:21 -0300

    actualizacion de url en routes

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -4,12 +4,13 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function NotFound() {
+  const url="/React_Routes/";
   return (
     <div className='d-flex flex-column align-items-center gap-3 pt-5'>
       <h2>Página no encontrada</h2>
       <Image src="https://amoradulce.com/wp-content/uploads/2022/09/91630428-748a-48b4-80aa-7d76d5a0c38f.jpg" thumbnail style={{width:'16rem'}}/>
       <Button className='bottonColor ' type="submit" variant='dark'>
-        <Link to='/' className='text-decoration-none text-white'>Volver al home.</Link> 
+        <Link to={url} className='text-decoration-none text-white'>Volver al home.</Link> 
       </Button>
     </div>
   )
