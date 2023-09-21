838164d716735c342103be299aaa098a129852a9
Pablo <emilio7b@gmail.com>
2023-09-20 22:53:37 -0300

    actualizacion

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -3,14 +3,14 @@ import { Container, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
-  const url="/React_Routes/";
+  const url="/React_Routes";
   return (
     <Navbar bg="dark" variant="dark" className="d-flex justify-content-between navbar navbar-expand-lg ">
         <Container className='d-flex gap-3 justify-content-start'>
             <Link to={url} className='text-decoration-none text-white'>
                 🏠 Home
             </Link>
-            <Link to={`${url}Contact`} className='text-decoration-none text-white'>
+            <Link to={`${url}/Contact`} className='text-decoration-none text-white'>
                 📗 Contacto
             </Link>
         </Container>
